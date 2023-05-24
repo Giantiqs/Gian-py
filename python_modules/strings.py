657414bc1167c5eaf841eb3e70cdf00448f8128c
gian <michaelgiantiqui3@gmail.com>
2023-05-25 00:44:48 +0800

    insert py files

---
+++ python_modules/strings.py
@@ -0,0 +1,15 @@
+def strings():
+    string = "Michael Gian M. Tiqui"
+
+    print(string.upper())
+    print(string.lower())
+
+    str_lst = string.split()
+    
+    print(str_lst)
+    
+    new_str = "".join("gian")
+
+    print(new_str)
+
+strings()
